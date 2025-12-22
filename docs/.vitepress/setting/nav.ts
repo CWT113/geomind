0a8b0bd410c2fb6b4ec4fd0ccb52ca094f6503af
liuxu@ev-image.com <liuxu@ev-image.com>
2025-12-22 09:45:00 +0800

    feat: 新增Java新特性集

--- docs/.vitepress/setting/nav.ts
+++ docs/.vitepress/setting/nav.ts
@@ -1,268 +1,272 @@
 export const nav = [
   {
-    text: "前端",
-    items: [
-      {
-        text: "CSS",
-        link: "/frontend/css/字体设置.md",
-      },
-      {
-        text: "JavaScript",
-        link: "/frontend/javascript/1.嫌隙小记.md",
-      },
-      {
-        text: "TypeScript",
-        link: "/a.md",
-      },
-    ],
+	text: "前端",
+	items: [
+	  {
+		text: "CSS",
+		link: "/frontend/css/字体设置.md",
+	  },
+	  {
+		text: "JavaScript",
+		link: "/frontend/javascript/1.嫌隙小记.md",
+	  },
+	  {
+		text: "TypeScript",
+		link: "/a.md",
+	  },
+	],
   },
 
   {
-    text: "Vue",
-    items: [
-      {
-        text: "全家桶",
-        items: [
-          {
-            text: "Vue3",
-            link: "/frontend/vue/1.组件传值.md",
-          },
-          {
-            text: "Pinia",
-            link: "/frontend/pinia/1.简介.md",
-          },
-          {
-            text: "Vue Router",
-            link: "/a.md",
-          },
-          {
-            text: "Vite",
-            link: "/a.md",
-          },
-          {
-            text: "Rollup",
-            link: "/a.md",
-          },
-        ],
-      },
-      {
-        text: "组件库",
-        items: [
-          {
-            text: "Element Plus",
-            link: "/a.md",
-          },
-        ],
-      },
-      {
-        text: "微前端",
-        items: [
-          {
-            text: "微前端",
-            link: "/frontend/MicroFrontend/qiankun.md",
-          },
-        ],
-      },
-    ],
+	text: "Vue",
+	items: [
+	  {
+		text: "全家桶",
+		items: [
+		  {
+			text: "Vue3",
+			link: "/frontend/vue/1.组件传值.md",
+		  },
+		  {
+			text: "Pinia",
+			link: "/frontend/pinia/1.简介.md",
+		  },
+		  {
+			text: "Vue Router",
+			link: "/a.md",
+		  },
+		  {
+			text: "Vite",
+			link: "/a.md",
+		  },
+		  {
+			text: "Rollup",
+			link: "/a.md",
+		  },
+		],
+	  },
+	  {
+		text: "组件库",
+		items: [
+		  {
+			text: "Element Plus",
+			link: "/a.md",
+		  },
+		],
+	  },
+	  {
+		text: "微前端",
+		items: [
+		  {
+			text: "微前端",
+			link: "/frontend/MicroFrontend/qiankun.md",
+		  },
+		],
+	  },
+	],
   },
 
   {
-    text: "React",
-    link: "/a.md",
+	text: "React",
+	link: "/a.md",
   },
 
   {
-    text: "Java",
-    items: [
-      {
-        text: "基础知识",
-        items: [
-          {
-            text: "Java基础",
-            link: "/a.md",
-          },
-          {
-            text: "数据结构和算法",
-            link: "/backend/java/data-structure/basic-structure/1.算法复杂度分析.md",
-          },
-          {
-            text: "JDBC",
-            link: "/a.md",
-          },
-          {
-            text: "Java Web",
-            link: "/a.md",
-          },
-          {
-            text: "Maven",
-            link: "/a.md",
-          },
-        ],
-      },
-      {
-        text: "Spring全家桶",
-        items: [
-          {
-            text: "Spring6",
-            link: "/backend/Java/Spring/Spring简介.md",
-          },
-          {
-            text: "Spring MVC",
-            link: "/backend/Java/SpringMVC/MVC理论基础.md",
-          },
-          {
-            text: "Spring Boot",
-            link: "/backend/Java/SpringBoot/基础使用.md",
-          },
-          {
-            text: "MyBatis",
-            link: "/backend/Java/Mybatis/基础使用.md",
-          },
-          {
-            text: "MyBatis Plus",
-            link: "/backend/Java/MybatisPlus/基础使用.md",
-          },
-        ],
-      },
-      {
-        text: "高级调优",
-        items: [
-          {
-            text: "JVM",
-            link: "/a.md",
-          },
-          {
-            text: "JUC",
-            link: "/a.md",
-          },
-        ],
-      },
-      {
-        text: "中间件",
-        items: [
-          {
-            text: "Kafka",
-            link: "/a.md",
-          },
-          {
-            text: "RabbitMQ",
-            link: "/a.md",
-          },
-          {
-            text: "Redis",
-            link: "/a.md",
-          },
-          {
-            text: "服务注册",
-            link: "/backend/java/discovery/consul/1.注册中心.md",
-          },
-        ],
-      },
-    ],
+	text: "Java",
+	items: [
+	  {
+		text: "基础知识",
+		items: [
+		  {
+			text: "Java基础",
+			link: "/a.md",
+		  },
+		  {
+			text: "Java新特性",
+			link: "/backend/java/java-feature/java8/收集器.md",
+		  },
+		  {
+			text: "JDBC",
+			link: "/a.md",
+		  },
+		  {
+			text: "Maven",
+			link: "/a.md",
+		  },
+		  {
+			text: "Java Web",
+			link: "/a.md",
+		  },
+		  {
+			text: "数据结构和算法",
+			link: "/backend/java/data-structure/basic-structure/1.算法复杂度分析.md",
+		  },
+		],
+	  },
+	  {
+		text: "Spring全家桶",
+		items: [
+		  {
+			text: "Spring6",
+			link: "/backend/Java/Spring/Spring简介.md",
+		  },
+		  {
+			text: "Spring MVC",
+			link: "/backend/Java/SpringMVC/MVC理论基础.md",
+		  },
+		  {
+			text: "Spring Boot",
+			link: "/backend/Java/SpringBoot/基础使用.md",
+		  },
+		  {
+			text: "MyBatis",
+			link: "/backend/Java/Mybatis/基础使用.md",
+		  },
+		  {
+			text: "MyBatis Plus",
+			link: "/backend/Java/MybatisPlus/基础使用.md",
+		  },
+		],
+	  },
+	  {
+		text: "高级调优",
+		items: [
+		  {
+			text: "JVM",
+			link: "/a.md",
+		  },
+		  {
+			text: "JUC",
+			link: "/a.md",
+		  },
+		],
+	  },
+	  {
+		text: "中间件",
+		items: [
+		  {
+			text: "Redis",
+			link: "/a.md",
+		  },
+		  {
+			text: "Kafka",
+			link: "/a.md",
+		  },
+		  {
+			text: "RabbitMQ",
+			link: "/a.md",
+		  },
+		  {
+			text: "Nacos",
+			link: "/backend/java/discovery/nacos/1.注册中心.md",
+		  },
+		],
+	  },
+	],
   },
 
   {
-    text: "数据库",
-    items: [
-      {
-        text: "MySQL",
-        link: "/a.md",
-      },
-      {
-        text: "PostgreSQL",
-        link: "/a.md",
-      },
-    ],
+	text: "数据库",
+	items: [
+	  {
+		text: "MySQL",
+		link: "/a.md",
+	  },
+	  {
+		text: "PostgreSQL",
+		link: "/a.md",
+	  },
+	],
   },
 
   {
-    text: "GIS",
-    items: [
-      {
-        text: "理论知识",
-        items: [
-          {
-            text: "基础理论",
-            link: "/a.md",
-          },
-        ],
-      },
-      {
-        text: "二维地图",
-        items: [
-          {
-            text: "Mapbox",
-            link: "/a.md",
-          },
-          {
-            text: "Open layers",
-            link: "/a.md",
-          },
-        ],
-      },
-      {
-        text: "三维地图",
-        items: [
-          {
-            text: "Cesium",
-            link: "/gis/Cesium/1.基础使用.md",
-          },
-          {
-            text: "Mars3d",
-            link: "/a.md",
-          },
-        ],
-      },
-      {
-        text: "图形化渲染",
-        items: [
-          {
-            text: "WebGL",
-            link: "/gis/webgl/1.WebGL概述.md",
-          },
-          {
-            text: "ThreeJS",
-            link: "/a.md",
-          },
-        ],
-      },
-      {
-        text: "服务托管",
-        items: [
-          {
-            text: "GeoServer",
-            link: "/a.md",
-          },
-        ],
-      },
-      {
-        text: "GIS工具库",
-        items: [
-          {
-            text: "GeoTools",
-            link: "/a.md",
-          },
-        ],
-      },
-    ],
+	text: "GIS",
+	items: [
+	  {
+		text: "理论知识",
+		items: [
+		  {
+			text: "基础理论",
+			link: "/a.md",
+		  },
+		],
+	  },
+	  {
+		text: "二维地图",
+		items: [
+		  {
+			text: "Mapbox",
+			link: "/a.md",
+		  },
+		  {
+			text: "Open layers",
+			link: "/a.md",
+		  },
+		],
+	  },
+	  {
+		text: "三维地图",
+		items: [
+		  {
+			text: "Cesium",
+			link: "/gis/Cesium/1.基础使用.md",
+		  },
+		  {
+			text: "Mars3d",
+			link: "/a.md",
+		  },
+		],
+	  },
+	  {
+		text: "图形化渲染",
+		items: [
+		  {
+			text: "WebGL",
+			link: "/gis/webgl/1.WebGL概述.md",
+		  },
+		  {
+			text: "ThreeJS",
+			link: "/a.md",
+		  },
+		],
+	  },
+	  {
+		text: "服务托管",
+		items: [
+		  {
+			text: "GeoServer",
+			link: "/a.md",
+		  },
+		],
+	  },
+	  {
+		text: "GIS工具库",
+		items: [
+		  {
+			text: "GeoTools",
+			link: "/a.md",
+		  },
+		],
+	  },
+	],
   },
 
   {
-    text: "服务端",
-    items: [
-      {
-        text: "Nginx",
-        link: "/a.md",
-      },
-      {
-        text: "Linux",
-        link: "/devops/linux/系统命令.md",
-      },
-      {
-        text: "Docker",
-        link: "/devops/docker/镜像与容器.md",
-      },
-    ],
+	text: "服务端",
+	items: [
+	  {
+		text: "Nginx",
+		link: "/a.md",
+	  },
+	  {
+		text: "Linux",
+		link: "/devops/linux/系统命令.md",
+	  },
+	  {
+		text: "Docker",
+		link: "/devops/docker/镜像与容器.md",
+	  },
+	],
   },
 
   { text: "听首歌", link: "http://music.alger.fun", noIcon: false },
